c66a76c76d730bfea51a8e386071c4206907a535
Zane Helton <zane99@me.com>
2016-08-10 18:38:09 -0400

    Update packages.sh

--- repo/packages.sh
+++ repo/packages.sh
@@ -2,6 +2,5 @@
 # dpkg-deb -bZgzip projects/[package-name] [output-folder]
 
 dpkg-deb -bZgzip projects/chineseskillhack debs
-dpkg-deb -bZgzip projects/liusoundboard debs
 dpkg-deb -bZgzip projects/cctimercountdown debs
 dpkg-deb -bZgzip projects/unsplashwallpaper debs
