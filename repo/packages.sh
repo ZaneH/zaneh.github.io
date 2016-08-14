96e3b648a392d9cce934f48c87bb4081b551129e
Zane Helton <zane99@me.com>
2016-08-13 20:14:36 -0400

    Added 'HotAsBalls'

--- repo/packages.sh
+++ repo/packages.sh
@@ -4,3 +4,4 @@
 dpkg-deb -bZgzip projects/chineseskillhack debs
 dpkg-deb -bZgzip projects/cctimercountdown debs
 dpkg-deb -bZgzip projects/unsplashwallpaper debs
+dpkg-deb -bZgzip projects/hotasballs debs
